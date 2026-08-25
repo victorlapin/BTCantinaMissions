ee6628ae154f3d923846b03628ae1f0daf939cc2
Victor Lapin <victor@slimroms.org>
2026-08-25 17:37:30 +0300

    domain: fix state serialization

--- src/Domain/State.cs
+++ src/Domain/State.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace BTCantinaMissions.Domain
 {
@@ -101,9 +102,11 @@ namespace BTCantinaMissions.Domain
         /// <summary>Player's taken tasks (Taken / ReadyToDeliver). Removed on delivery.</summary>
         public List<TaskInstance> ActiveTasks { get; internal set; } = new List<TaskInstance>();
 
+        [JsonIgnore]
         public IEnumerable<TaskInstance> InProgress =>
             ActiveTasks.Where(t => t.State == TaskState.Taken);
 
+        [JsonIgnore]
         public IEnumerable<TaskInstance> Deliverable =>
             ActiveTasks.Where(t => t.State == TaskState.ReadyToDeliver);
 
